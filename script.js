0a8da473edaf9abc723c2800d63ad78b79440b17
0908-bit <fogarginevra@gmail.com>
2025-06-17 10:02:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const fighterList = document.getElementById('fighter-list');
     const fighterStats = document.getElementById('fighter-stats');
 
-    fetch('data/fighters.json')
+    fetch('https://0908-bit.github.io/mma-ufc-news/data/fighters.json')
         .then(response => response.json())
         .then(data => {
             // Popola la lista fighter nella homepage
